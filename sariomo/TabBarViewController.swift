d2660590741a939703d73cceccd0337ab78ab5f0
git-air <i.air.run@gmail.com>
2019-10-27 06:30:07 +0900

    プロジェクトを作成し、タブバーを実装

---
+++ sariomo/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  sariomo
+//
+//  Created by AIRU ISHIKURA on 2019/10/27.
+//  Copyright © 2019 AIRU ISHIKURA. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().tintColor = MyColor.tomato
+    }
+
+}
---
+++ sariomo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sariomo
+//
+//  Created by AIRU ISHIKURA on 2019/10/27.
+//  Copyright © 2019 AIRU ISHIKURA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
